40158072cfe357bca3aa598fabe6a419ff7834ff
Nuno Custódio <nuno.fcustodio@gmail.com>
2020-12-14 15:26:00 +0000

    .

--- weather-app/src/components/Days.js
+++ weather-app/src/components/Days.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import dayPic from '../assets/day.svg'
 
 const Days = ({ days }) => {
   if (days) {
     return (
       <>
+      <div class="days">
         {days.response.map((day, i) => (
           <span className='day' key={i}>
-            <img src={'../assets/day.svg'} alt='day' />
+            <img src={dayPic} alt='day' />
+            <p>Min: {day.tMin} Max: {day.tMax}</p>
           </span>
+         
         ))}
+         </div>
       </>
     );
-    //console.log(days);
   }
   return null;
 };
